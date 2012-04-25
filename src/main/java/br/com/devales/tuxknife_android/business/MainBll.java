dd587794ce6f2b50505d4844ca05ace331571c7a
Junior Ales <sela-roinuj@hotmail.com>
2012-04-25 19:42:47 -0300

    Android Arch test

---
+++ src/main/java/br/com/devales/tuxknife_android/business/MainBll.java
@@ -0,0 +1,12 @@
+package br.com.devales.tuxknife_android.business;
+
+public class MainBll {
+
+	public String getDisco() {
+		// will communicate with core and bring the data
+		return "ola que tal";
+	}
+	
+	
+
+}
